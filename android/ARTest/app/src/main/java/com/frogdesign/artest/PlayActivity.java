d48ac67fbbb6c8172e270abd5a18c3445a421fec
Emanuele Di Saverio <emanuele.disaverio@gmail.com>
2016-01-11 18:41:45 +0100

    tracking ok, kotlin ok, but position completely wrong

--- android/ARTest/app/src/main/java/com/frogdesign/artest/PlayActivity.java
+++ android/ARTest/app/src/main/java/com/frogdesign/artest/PlayActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.frogdesign.akart.model.Car;
@@ -186,5 +187,19 @@ public class PlayActivity extends AppCompatActivity {
 //            float[] canaliPercettivi = ARToolKit.getInstance().queryMarkerTransformation(markerIDHijo);
 //            printMatrix(canaliPercettivi);
 //        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }
---
+++ android/ARTest/app/src/main/kotlin/com/frogdesign/akart/model/Car.kt
@@ -0,0 +1,63 @@
+package com.frogdesign.akart.model;
+
+import org.artoolkit.ar.base.ARToolKit
+
+data class Position(var x: Float, var y: Float, var z: Float) {
+    operator infix fun divAssign(d: Int) {
+        x /= d;
+        y /= d;
+        z /= d;
+    }
+}
+
+/**
+ * In the a-kart game the car is identified by two marker glued on the rear
+ * of the cars, one on the left and one on the right of the car
+ *
+ */
+data class Car(val id: String, val lrMarkers: Pair<Int, Int>) {
+    var leftAR: Int = -1;
+    var rightAR: Int = -1;
+
+    fun isDetected(arScene: ARToolKit): Boolean = isLeftMarkerVisible(arScene) || isRightMarkerVisible(arScene)
+
+    private fun isLeftMarkerVisible(arScene: ARToolKit) = leftAR >= 0 && arScene.queryMarkerVisible(leftAR)
+    private fun isRightMarkerVisible(arScene: ARToolKit) = rightAR >= 0 && arScene.queryMarkerVisible(rightAR)
+
+    fun estimatePosition(arScene: ARToolKit): Position {
+        var pos : Position = Position(0f,0f,0f);
+        if (!isDetected(arScene)) return pos;
+        var sides : Int = 0;
+        if(isLeftMarkerVisible(arScene)) {
+            var matrix : FloatArray = arScene.queryMarkerTransformation(leftAR)
+            sides++;
+            pos.x += matrix[12];
+            pos.y += matrix[13];
+            pos.z += matrix[14];
+        }
+
+        if(isRightMarkerVisible(arScene)) {
+            var matrix : FloatArray = arScene.queryMarkerTransformation(rightAR)
+            sides++;
+            pos.x += matrix[12];
+            pos.y += matrix[13];
+            pos.z += matrix[14];
+        }
+
+        pos /= sides;
+
+        //scaled to nullify depth
+        var depth : Float = pos.z / -500;
+        pos.x /= depth;
+        pos.y /= depth;
+        return pos;
+    }
+}
+
+object Cars {
+    @JvmField
+    public val all = listOf(
+            Car("taxiguerrilla", Pair(0, 1)),
+            Car("gargamella", Pair(2, 3))
+    );
+}
