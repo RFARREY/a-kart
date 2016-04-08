74d4501a425e10f0fc33c07cec0d0367db7c0bd3
Emanuele Di Saverio <emanuele.disaverio@gmail.com>
2016-04-08 17:18:54 +0200

    UI for the app complete

--- android/a-kart/app/src/main/kotlin/com/frogdesign/akart/view/AimView.kt
+++ android/a-kart/app/src/main/kotlin/com/frogdesign/akart/view/AimView.kt
@@ -5,13 +5,13 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import android.view.animation.LinearInterpolator
+import android.widget.Scroller
 import com.frogdesign.akart.R
 import com.frogdesign.akart.util.ResourcesCompatInstance
+import com.frogdesign.akart.util.clamp
 import com.frogdesign.akart.util.dpToPx
-import timber.log.Timber
 import uk.co.chrisjenx.calligraphy.TypefaceUtils
-
+import kotlin.properties.Delegates
 
 class AimView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : View(context, attrs, defStyleAttr, defStyleRes) {
     companion object {
@@ -29,21 +29,25 @@ class AimView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defSty
     private val paint: Paint
     private val textPaint: Paint
     private var rotationAnim: ValueAnimator = ValueAnimator.ofFloat(0f, 0f)
+    private var scroller: Scroller  by Delegates.notNull()
     var targetedId: String? = null
 
     fun horizonRotate(c: Float) {
-        rotationAnim.cancel()
-        rotationAnim = ValueAnimator.ofFloat(actualRotation, c)
-        rotationAnim.addUpdateListener { ev ->
-            actualRotation = ev.animatedValue as Float
-            invalidate()
-        }
-        rotationAnim.interpolator = LinearInterpolator()
-        rotationAnim.start()
+        //        rotationAnim.cancel()
+        //        rotationAnim = ValueAnimator.ofFloat(actualRotation, c)
+        //        rotationAnim.addUpdateListener { ev ->
+        //            actualRotation = ev.animatedValue as Float
+        //            invalidate()
+        //        }
+        //        rotationAnim.interpolator = LinearInterpolator()
+        //        rotationAnim.start()
+        targetRotation = c
+        invalidate()
     }
 
     private var targetRotation = 0f
     private var actualRotation = 0f
+    private var actualVelocity = 0f
 
     init {
         if (context != null) {
@@ -52,6 +56,7 @@ class AimView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defSty
             stroke = resources.getDimension(R.dimen.btn_stroke)
             color = ResourcesCompatInstance.getColor(resources, R.color.militaryGreen, null)
             textSize = dpToPx(context, 12f).toFloat()
+            scroller = Scroller(context)
         }
         paint = Paint()
         paint.style = Paint.Style.STROKE
@@ -87,7 +92,7 @@ class AimView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defSty
         var cx = (width / 2).toFloat()
         var cy = (height / 2).toFloat()
         targetedId = null
-        var targeted =  false
+        var targeted = false
         for ((k, b) in points.entries) {
             if (b.x > Float.MIN_VALUE) {
                 //Timber.i("Drawing %s, %s", k, b)
@@ -107,6 +112,16 @@ class AimView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defSty
             }
         }
         drawAim(canvas, cx, cy, targeted)
+        var delta = targetRotation - actualRotation
+        if (Math.abs(delta) > 0) {
+            actualVelocity = delta / 6
+
+            actualVelocity = clamp(actualVelocity, -10f, +10f)
+            if (Math.abs(actualVelocity) > 1) {
+                actualRotation += actualVelocity
+                invalidate()
+            }
+        }
     }
 
     private fun drawAim(canvas: Canvas, cx: Float, cy: Float, targeted: Boolean) {
